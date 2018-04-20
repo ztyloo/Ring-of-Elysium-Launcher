47c636142d287d844bcd3f34a7490ec20839e03f
RoBit <sasha.robit@gmail.com>
2018-04-20 19:23:24 +0300

    Init

---
+++ RingOfElysiumLauncher/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+
+namespace RingOfElysiumLauncher {
+
+	public partial class App : Application {
+
+		private void Application_Startup(object sender, StartupEventArgs e) {
+			MainWindow mw = new MainWindow(e.Args);
+			mw.Show();
+		}
+
+	}
+}
---
+++ RingOfElysiumLauncher/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace RingOfElysiumLauncher {
+
+	public partial class MainWindow : Window {
+
+		public MainWindow(string[] agrs) {
+			InitializeComponent();
+		}
+	}
+}
