96f093ff6f54d76395133496d612ae59ac2fac42
Александр <sasha.robit@gmail.com>
2018-04-24 01:29:54 +0300

    Создание ярлыка, запись всех данных

--- RingOfElysiumLauncherInstaller/MainWindow.xaml.cs
+++ RingOfElysiumLauncherInstaller/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Windows;
 using System.ComponentModel;
 
+using IWshRuntimeLibrary;
 using Microsoft.Win32;
 
 using Octokit;
@@ -30,6 +31,8 @@ namespace RingOfElysiumLauncherInstaller {
 
         private void InstallButton_Click(object sender, RoutedEventArgs e) {
             Install();
+
+            InstallButton.Content = "PLEASE WAIT";
             InstallButton.IsEnabled = false;
         }
 
@@ -37,15 +40,23 @@ namespace RingOfElysiumLauncherInstaller {
         private async void Install() {
             // Получение ссылки на последнюю версию
             var client = new GitHubClient(new ProductHeaderValue("Ring-of-Elysium-Launcher"));
-            var releases = await client.Repository.Release.GetAll("RoBit666", "Ring-of-Elysium-Launcher");
-            var urlDownload = releases[0].Assets[0].BrowserDownloadUrl;
+            var releases = await client.Repository.Release.GetLatest("RoBit666", "Ring-of-Elysium-Launcher");
 
-            WebClient webload = new WebClient();
-            webload.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadCompleted);
-            webload.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            foreach(ReleaseAsset ra in releases.Assets) {
+                if(ra.Name == "RingOfElysiumLauncher.exe") {
 
-            // Загружаем файл
-            webload.DownloadFileAsync(new Uri(urlDownload), Path.GetDirectoryName(PathTextBox.Text) + "/DownloadLaucher");
+                    // Создание веб-клиента
+                    WebClient webload = new WebClient();
+                    webload.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadCompleted);
+                    webload.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                    // Загружаем файл
+                    webload.DownloadFileAsync(new Uri(ra.BrowserDownloadUrl), Path.GetDirectoryName(PathTextBox.Text) + "/DownloadLaucher");
+
+                    break;
+                }
+            }
+
         }
 
         // Обработчик прогресса
@@ -60,18 +71,25 @@ namespace RingOfElysiumLauncherInstaller {
                 MessageBox.Show(e.Error.Message, "Error");
             } else {
 
-                if(!File.Exists(Path.GetDirectoryName(PathTextBox.Text) + "\\roe_game.exe")) { 
-                    File.Move(PathTextBox.Text, Path.GetDirectoryName(PathTextBox.Text) + "\\roe_game.exe");
+                if(!System.IO.File.Exists(Path.GetDirectoryName(PathTextBox.Text) + "\\roe_game.exe")) {
+                    System.IO.File.Move(PathTextBox.Text, Path.GetDirectoryName(PathTextBox.Text) + "\\roe_game.exe");
                 } else {
-                    File.Delete(PathTextBox.Text);
+                    System.IO.File.Delete(PathTextBox.Text);
                 }
 
-                File.Move(Path.GetDirectoryName(PathTextBox.Text) + "\\DownloadLaucher", PathTextBox.Text);
+                System.IO.File.Move(Path.GetDirectoryName(PathTextBox.Text) + "\\DownloadLaucher", PathTextBox.Text);
 
                 // Настройки лаунчера
                 RegistryKey launcherSettings = Registry.CurrentUser.OpenSubKey("RoELauncher", true); 
                 if (launcherSettings == null) launcherSettings = Registry.CurrentUser.CreateSubKey("RoELauncher", RegistryKeyPermissionCheck.ReadWriteSubTree);
                 launcherSettings.SetValue("PathToGame", Path.GetDirectoryName(PathTextBox.Text) + "\\roe_game.exe", RegistryValueKind.String);
+
+                // Ярлык
+                WshShell shell = new WshShell();
+                string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Ring of Elysium.lnk";
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+                shortcut.TargetPath = PathTextBox.Text;
+                shortcut.Save();
 
                 InstallButton.Content = "INSTALLATION COMPLETED";
             }
